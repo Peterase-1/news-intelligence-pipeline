8585159bff8f30aba8174fc7b3f02b08b60ca22a
Petros Asegid <pasegid@gmail.com>
2026-01-12 22:53:26 +0300

    Restore Django API files after git crash

--- api/news/serializers.py
+++ api/news/serializers.py
@@ -9,6 +9,3 @@ class NewsArticleSerializer(serializers.ModelSerializer):
             'id', 'url', 'title', 'source', 'published_at', 
             'sentiment_label', 'sentiment_score', 'created_at'
         ]
-        # We exclude 'embedding' and 'body_text' from list view to save bandwidth
-        # but maybe we want body_text in detailed view?
-        # For now, let's include basic info.
